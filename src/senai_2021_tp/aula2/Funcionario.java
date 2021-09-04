1337f8358e156eaa0858b93d6235c0ee4436083a
cybermaique <maiqued.18@gmail.com>
2021-09-04 02:06:58 -0300

    two

---
+++ src/senai_2021_tp/aula2/Funcionario.java
@@ -0,0 +1,12 @@
+package senai_2021_tp.aula2;
+
+
+public class Funcionario {
+
+	public String nome;
+	public int idade; 
+	public String dataNascimento; 
+	public float salario; 
+            
+    
+}
